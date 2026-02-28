1c44506e1982a9a6db9b360635926422ff973a5f
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-02-28 12:24:18 +0100

    Implementamos el servicio con el método getAllProducts() y el insertProduct()

--- ejercicio-amazon-angular/src/app/service/products.sevice.ts
+++ ejercicio-amazon-angular/src/app/service/products.sevice.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { IProduct } from '../interfaces/iproduct.interface';
+import { PRODUCTS } from '../db/products.db';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsSevice {
-  
+  private products:IProduct[] = PRODUCTS;
+
+  getAllProducts(): IProduct[] {
+    return this.products;
+  }
+
+
+  insertProduct(newProduct: IProduct): void {
+     this.products.push(newProduct);
+  }; 
+
 }
