8884afbdff7f19d6f11b5a051f0dd5a58de04e74
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-02-28 12:45:42 +0100

    Importamos FormsModule e inyectamos el servicio en el componente Formulario

--- ejercicio-amazon-angular/src/app/components/product-form/product-form.ts
+++ ejercicio-amazon-angular/src/app/components/product-form/product-form.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductsSevice } from '../../service/products.sevice';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-form.html',
   styleUrl: './product-form.css',
 })
 export class ProductForm {
+  productService = inject(ProductsSevice);
 
 }
