3fd3f1a7d6fd2f588f0e06902cc22a2f12f58975
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-02-28 12:07:21 +0100

    Creamos 4 componentes: 1) productForm, 2) productList, 3) productCard,  4) totalCart

---
+++ ejercicio-amazon-angular/src/app/components/total-cart/total-cart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-total-cart',
+  imports: [],
+  templateUrl: './total-cart.html',
+  styleUrl: './total-cart.css',
+})
+export class TotalCart {
+
+}
