04dfa3de4f51ecad00acff212cd5a7bd0dcaca2b
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-02-28 12:36:40 +0100

    Maquetación inicial de la estructura de la pagina

--- ejercicio-amazon-angular/src/app/app.ts
+++ ejercicio-amazon-angular/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductForm } from './components/product-form/product-form';
+import { ProductList } from './components/product-list/product-list';
+import { TotalCart } from './components/total-cart/total-cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductForm, ProductList, TotalCart],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ejercicio-amazon-angular');
+
+
+
 }
